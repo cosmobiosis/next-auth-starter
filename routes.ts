b37039633bfaa18a764c0e34647da12e56934a17
Mohamad Salimi <contact@salimi.my>
2024-04-13 02:41:39 +0800

    Update routes.ts

--- routes.ts
+++ routes.ts
@@ -3,14 +3,14 @@
  * These routes does not require authentication
  * @type {string[]}
  */
-export const publicRoutes = ['/', '/auth/email-verification'];
+export const publicRoutes: string[] = ['/', '/auth/email-verification'];
 
 /**
  * An array of routes that are used for authentication
  * These routes will redirect signed in users to /settings
  * @type {string[]}
  */
-export const authRoutes = [
+export const authRoutes: string[] = [
   '/auth/sign-in',
   '/auth/sign-up',
   '/auth/error',
@@ -23,10 +23,10 @@ export const authRoutes = [
  * Routes that start with this prefix are used for API authentication purposes
  * @type {string}
  */
-export const apiAuthPrefix = '/api/auth';
+export const apiAuthPrefix: string = '/api/auth';
 
 /**
  * The default redirect path after logging in
  * @type {string}
  */
-export const DEFAULT_SIGNIN_REDIRECT = '/settings';
+export const DEFAULT_SIGNIN_REDIRECT: string = '/settings';
